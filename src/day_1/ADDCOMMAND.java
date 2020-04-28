c3b711e7ce0d34fcd3fefddc20eb02b15e4172ef
Ismail Gercek <ismailgercek@Ismails-MacBook-Pro.local>
2020-04-28 15:41:04 -0400

    add HappyBirthday.java

---
+++ src/day_1/ADDCOMMAND.java
@@ -0,0 +1,11 @@
+package day_1;
+
+public class ADDCOMMAND {
+
+    // I want to try git add
+    public static void main(String[] args) {
+
+        // I made chaned when i created agdd comment
+    }
+
+}
